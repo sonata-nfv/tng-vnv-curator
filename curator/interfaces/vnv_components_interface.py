0cee600d89731477b61e9b82763f5a5f5a8caec5
srjuanlu <jdelacruz@cttc.es>
2019-05-28 18:04:44 +0200

    Executor`s API for cancelation uses DELETE instead of POST

--- curator/interfaces/vnv_components_interface.py
+++ curator/interfaces/vnv_components_interface.py
@@ -573,7 +573,7 @@ class ExecutorInterface(Interface):
         url = '/'.join([self.base_url, self.api, self.version, 'test-executions', test_uuid, 'cancel'])
         headers = {"Content-type": "application/json"}
         try:
-            response = requests.post(url, headers=headers, json=data)
+            response = requests.delete(url, headers=headers, json=data)
             _LOG.debug(f'Rstatus: {response.status_code}')
             _LOG.debug(f'Rdata: {response.raw}')
             if response.status_code == 200:  # and not response.json()['error']:
